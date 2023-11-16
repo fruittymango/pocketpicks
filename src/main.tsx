ee0862f537d0a0c571d53b248dafca7a0c22ae9f
Isaac Mogano <kaymogano@gmail.com>
2023-11-16 13:05:29 +0200

    Added page not found component

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './main.css';
 
 const ComingSoon = () => {
@@ -48,8 +49,58 @@ const ComingSoon = () => {
   );
 }
 
+const PageNotFound = () => {
+  return(
+    <>
+    <div style={{position:'absolute', left:'1%', padding:'0rem 1rem',}}>
+      <h1 className='app-title' style={{marginTop:'2rem'}}><strong>Pocket</strong><span>`ICKS</span></h1>
+    </div>
+
+    <div className="content">
+      
+      <div className='flex-center'>
+        <h1>404 - Page not found</h1>
+      </div>
+
+      <div className='horizontal-bar'>
+      </div>
+
+      <div className='menu-bg'>
+        <p className='menu-item bar'>
+          Dashboard
+        </p>
+        <p className='menu-item bar'>
+          Movies
+        </p>
+        <p className='menu-item'>
+          Series
+        </p>
+      </div>
+
+      <div className='menu-bg'>
+        <div className='flex-space-between'>
+          <p className='slogan'>Cinematic picks in your pocket..</p>
+       </div>
+      </div>
+    </div>
+
+
+    <div className='footer'><h4>&copy; Acsono 2023 <span>
+      <img src='/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg' height="20rem" alt='The movie database logo'/>
+    </span></h4>
+    </div>
+
+  </>
+  );
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ComingSoon/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ComingSoon/>} /> 
+        <Route path="/*" element={<PageNotFound/>} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 );
