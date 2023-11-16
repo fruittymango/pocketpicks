8105e550a824d798f7f187ef4ec88a530deac8bb
Isaac Mogano <kaymogano@gmail.com>
2023-11-16 20:19:09 +0200

    Moved the two available pages to their own files and folders.
    Decision: Styling for pages will now reside inside their respective folder. This makes it easier to read and understand the styling of each page with out having to go back and forth.

---
+++ src/Pages/ComingSoon/index.tsx
@@ -0,0 +1,40 @@
+import './index.css';
+const ComingSoon = () => {
+    return(
+      <>
+        <div className='logo-bg'>
+          <h1 className='logo'><strong>Pocket</strong><span>`ICKS</span></h1>
+        </div>
+  
+        <div className="content">
+          
+          <h2 className="main-title">Coming Soon</h2>
+  
+          <div className='horizontal-bar'>
+          </div>
+  
+          <div className='sub-title-wrapper'>
+            <p className='menu-item bar'>
+              Dashboard
+            </p>
+            <p className='menu-item bar'>
+              Movies
+            </p>
+            <p className='menu-item'>
+              Series
+            </p>
+          </div>
+          <p className='slogan'>Cinematic picks in your pocket..</p>
+        </div>
+  
+  
+        <div className='footer'><h4>&copy; Acsono 2023 <span>
+          <img src='/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg' height="20rem" alt='The movie database logo'/>
+        </span></h4>
+        </div>
+  
+      </>
+    );
+}
+
+export default ComingSoon;
--- src/main.tsx
+++ src/main.tsx
@@ -1,105 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './main.css';
-
-const ComingSoon = () => {
-
-  return(
-    <>
-      <div style={{position:'absolute', left:'1%', padding:'0rem 1rem',}}>
-        <h1 className='app-title' style={{marginTop:'2rem'}}><strong>Pocket</strong><span>`ICKS</span></h1>
-      </div>
-
-      <div className="content">
-        
-        <div className='flex-center'>
-          <h1>Coming Soon</h1>
-        </div>
-
-        <div className='horizontal-bar'>
-        </div>
-
-        <div className='menu-bg'>
-          <p className='menu-item bar'>
-            Dashboard
-          </p>
-          <p className='menu-item bar'>
-            Movies
-          </p>
-          <p className='menu-item'>
-            Series
-          </p>
-        </div>
-
-        <div className='menu-bg'>
-          <div className='flex-space-between'>
-            <p className='slogan'>Cinematic picks in your pocket..</p>
-         </div>
-        </div>
-      </div>
-
-
-      <div className='footer'><h4>&copy; Acsono 2023 <span>
-        <img src='/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg' height="20rem" alt='The movie database logo'/>
-      </span></h4>
-      </div>
-
-    </>
-  );
-}
-
-const PageNotFound = () => {
-  return(
-    <>
-    <div style={{position:'absolute', left:'1%', padding:'0rem 1rem',}}>
-      <h1 className='app-title' style={{marginTop:'2rem'}}><strong>Pocket</strong><span>`ICKS</span></h1>
-    </div>
-
-    <div className="content">
-      
-      <div className='flex-center'>
-        <h1>404 - Page not found</h1>
-      </div>
-
-      <div className='horizontal-bar'>
-      </div>
-
-      <div className='menu-bg'>
-        <p className='menu-item bar'>
-          Dashboard
-        </p>
-        <p className='menu-item bar'>
-          Movies
-        </p>
-        <p className='menu-item'>
-          Series
-        </p>
-      </div>
-
-      <div className='menu-bg'>
-        <div className='flex-space-between'>
-          <p className='slogan'>Cinematic picks in your pocket..</p>
-       </div>
-      </div>
-    </div>
-
-
-    <div className='footer'><h4>&copy; Acsono 2023 <span>
-      <img src='/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg' height="20rem" alt='The movie database logo'/>
-    </span></h4>
-    </div>
-
-  </>
-  );
-}
+import PageNotFound from './Pages/404/index';
+import ComingSoon from './Pages/ComingSoon';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<ComingSoon/>} /> 
-        <Route path="/*" element={<PageNotFound/>} />
+        <Route path="/*" element={<PageNotFound/>} /> 
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
