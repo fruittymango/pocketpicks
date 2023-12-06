d6a4c0d7d02ad493a9ffc67ab166cf00b649839c
Isaac Mogano <kaymogano@gmail.com>
2023-12-06 14:59:52 +0200

    Added page not found screen along with the styling attributes

--- src/Pages/404/index.tsx
+++ src/Pages/404/index.tsx
@@ -1,3 +1,5 @@
+import './index.css';
+
 const PageNotFound = () => {
     return(
       <>
@@ -5,7 +7,7 @@ const PageNotFound = () => {
           <h1 className='logo'><strong>Pocket</strong><span>`ICKS</span></h1>
         </div>
   
-        <div className="content">
+        <div className="notfound-content">
           
           <h2 className="main-title">404 - Page Not Found</h2>
   
@@ -13,25 +15,24 @@ const PageNotFound = () => {
           </div>
   
           <div className='sub-title-wrapper'>
-            <p className='menu-item bar'>
-              Dashboard
-            </p>
-            <p className='menu-item bar'>
-              Movies
-            </p>
-            <p className='menu-item'>
-              Series
-            </p>
+            <a href='/Dashboard'>
+              <p className='menu-item bar'>
+                Dashboard
+              </p>
+            </a>
+            <a href='/Movies'>
+              <p className='menu-item bar'>
+                Movies
+              </p>
+            </a>
+            <a href='/TV-Shows'>
+              <p className='menu-item'>
+                Series
+              </p>
+            </a>
           </div>
           <p className='slogan'>Cinematic picks in your pocket..</p>
-        </div>
-  
-  
-        <div className='footer'><h4>&copy; Acsono 2023 <span>
-          <img src='/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg' height="20rem" alt='The movie database logo'/>
-        </span></h4>
-        </div>
-  
+        </div>  
       </>
     );
 }
