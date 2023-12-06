dab3e561ea78f094c8179a8870124e102cd7bf55
Isaac Mogano <kaymogano@gmail.com>
2023-12-06 15:31:05 +0200

    Added routing to the pages added

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import PageNotFound from './Pages/404/index';
-import ComingSoon from './Pages/ComingSoon';
+
+
+import SeeMorePage from './Pages/SeeMore';
+import TVShowsPage from './Pages/TVShows';
+import Dashboard from './Pages/Dashboard';
+import Movies from './Pages/Movies';
+import './main.css';
+
+import PageNotFound from './Pages/404';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<ComingSoon/>} /> 
-        <Route path="/*" element={<PageNotFound/>} /> 
+        <Route path="/" element={<Dashboard/>} />
+        <Route path="/Dashboard" element={<Dashboard/>} /> 
+        <Route path="/Movies" element={<Movies/>} />  
+        <Route path="/TV-Shows" element={<TVShowsPage/>} />  
+        <Route path="/See-More/:title" element={<SeeMorePage/>} /> 
+        <Route path="*" element={<PageNotFound/>} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
